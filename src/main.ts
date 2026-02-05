32877cdcab24caaddd627c238185700021cd138a
work <work@ixiaozi.cn>
2026-02-05 23:04:57 +0800

    *

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import * as process from "process";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,        // 自动删除非 DTO 中的属性
